1816ccd72b7b2e1cc564548b7f51c946d1e18cc4
gabriel80546 <gabriel80546@gmail.com>
2018-04-08 21:16:01 -0300

    Adicionado a funcao socket no codigo

--- server.c
+++ server.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 int main()
 {
-    printf("oi\n");
+    int socketFileDescriptor;
+    int i;
+    for(i=0;i<20;i++)
+    {
+        printf("Vai A merda %i\n",i);
+    }
+
+    socketFileDescriptor = socket(AF_INET ,SOCK_STREAM , 0);
+
+    if(socketFileDescriptor < 0)
+    {
+        fprintf(stderr,"Erro Ao iniciar Socket");
+        exit(0);
+    }
+
+
+    return 0;
 }
